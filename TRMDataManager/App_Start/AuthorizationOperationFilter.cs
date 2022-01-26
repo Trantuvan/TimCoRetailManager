80e1a0b584d1869d94e1856d6c6ed10ad8c3071d
trantuvan <trantuvan.kan@gmail.com>
2022-01-26 21:26:14 +0700

    Add Swagger Documentation to API
    Adds SwashBulker/Swagger to API for documenting and
    testing

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // for get all endpoint dont have params
+            if (operation.parameters == null)
+            {
+                //create a list
+                operation.parameters = new List<Parameter>();
+            }
+            //make sure have a list and populate with: 
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
