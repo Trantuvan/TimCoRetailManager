c67409efcb5568c97b865d8471e535c3423ad437
trantuvan <trantuvan.kan@gmail.com>
2022-01-27 21:10:19 +0700

    Add WPF User Interface for Retail Management

---
+++ TRMDesktopUI/Bootstraper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstraper : BootstrapperBase
+    {
+        public Bootstraper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
