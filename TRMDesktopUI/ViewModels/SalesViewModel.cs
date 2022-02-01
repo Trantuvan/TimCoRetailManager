67ed4b449e2cabcc2c29e34fe4e26331a8d68b94
trantuvan <trantuvan.kan@gmail.com>
2022-02-01 21:38:52 +0700

    Add Sales Page
    Adds the sales page XAML and the supporting.
    View Model. Just scaffolds out the VM, does not add actual logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,127 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        private string _itemQuantity;
+        private BindingList<string> _cart;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                // BindingList auto bind value
+                // Notify here in case of override entire list => Products fire
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public string SubToTal
+        {
+            get
+            {
+                //TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string ToTal
+        {
+            get
+            {
+                //TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                //TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //make sure sth is selected
+                //make sure there is an item quantity
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //make sure sth is selected
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //make sure sth is in the cart
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+        }
+
+    }
+}
