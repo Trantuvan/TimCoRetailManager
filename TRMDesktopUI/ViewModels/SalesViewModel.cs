e872229d8a91c6a315c604c08e2d96edf135a74f
trantuvan <trantuvan.kan@gmail.com>
2022-02-04 21:15:51 +0700

    Get Add to Cart Working
    Adds the functionality to add items to the cart, get the subtotal, and update the quantities.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -13,9 +13,10 @@ namespace TRMDesktopUI.ViewModels
     public class SalesViewModel : Screen
     {
         private BindingList<ProductModel> _products;
-        private int _itemQuantity;
-        private BindingList<ProductModel> _cart;
+        private int _itemQuantity = 1;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
         private IProductEndpoint _productEndpoint;
+        private ProductModel _selectedProduct;
 
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
@@ -46,6 +47,18 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
         public int ItemQuantity
         {
             get { return _itemQuantity; }
@@ -53,11 +66,13 @@ namespace TRMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                // after ItemQuantity Changes check to make sure CanAddToCart
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
 
-        public BindingList<ProductModel> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -67,12 +82,18 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        public string SubToTal
+        public string SubTotal
         {
             get
             {
                 //TODO - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
             }
         }
 
@@ -100,8 +121,12 @@ namespace TRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-                //make sure sth is selected
+                //make sure sth is selected (QuanityInStock >= ItemQuantity)
                 //make sure there is an item quantity
+                if (ItemQuantity >= 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -109,6 +134,29 @@ namespace TRMDesktopUI.ViewModels
 
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK - to trick there is the new object of CartItemModel 
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -125,6 +173,7 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
